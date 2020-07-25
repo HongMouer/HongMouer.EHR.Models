e22c428d17523779c0fba37928a9754ca0924f9a
HongMouer <HongMouer@Gmail.com>
2020-07-25 20:37:38 +0800

    Add project files.

---
+++ HongMouer.EHR.Models/Enum/EnumSex.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace HongMouer.EHR.Models
+{
+
+    /// <summary>
+    /// 性别标准 GB/T 2261.1-2003 人的性别代码
+    /// </summary>
+    public enum EnumSex
+    {
+
+        /// <summary>
+        /// 未知的性别
+        /// </summary>
+        [Description("未知的性别")]
+        未知 = 0,
+
+        /// <summary>
+        /// 男性
+        /// </summary>
+        [Description("男性")]
+        男性 = 1,
+
+        /// <summary>
+        /// 女性
+        /// </summary>
+        [Description("女性")]
+        女性 = 2,
+
+        /// <summary>
+        /// 未说明
+        /// </summary>
+        [Description("未说明")]
+        未说明 = 9
+    }
+}
