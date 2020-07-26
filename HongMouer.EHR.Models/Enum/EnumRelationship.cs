47cf4d80dcf84946a692fd1b46996a6280636b66
HongMouer <HongMouer@Gmail.com>
2020-07-27 00:08:04 +0800

    Add:       1、学位代码 GB/T 6864       2、职称/职务代码   GB/T 8561       3、所学专业代码  GB/T 16835       4、患者与本人关系代码  GB/T 4761       5、单位隶属关系代码  GB/T 12404

---
+++ HongMouer.EHR.Models/Enum/EnumRelationship.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HongMouer.EHR.Models
+{
+
+    /// <summary>
+    /// 患者与本人关系代码  GB/T 4761
+    /// </summary>
+    [Serializable]
+    public enum EnumRelationship
+    {
+        /// <summary>
+        /// 本人
+        /// </summary>
+        本人 = 0,
+
+        /// <summary>
+        /// 户主
+        /// </summary>
+        户主 = 0101,
+
+        /// <summary>
+        /// 配偶
+        /// </summary>
+        配偶 = 1,
+
+        /// <summary>
+        /// 子
+        /// </summary>
+        子 = 2,
+
+        /// <summary>
+        /// 孙子
+        /// </summary>
+        孙子 = 4,
+
+        /// <summary>
+        /// 孙女
+        /// </summary>
+        孙女 = 402,
+
+        /// <summary>
+        /// 外孙子
+        /// </summary>
+        外孙子 = 403,
+
+        /// <summary>
+        /// 外孙女
+        /// </summary>
+        外孙女 = 401,
+
+        /// <summary>
+        /// 父母
+        /// </summary>
+        父母 = 5,
+
+        /// <summary>
+        /// 祖父母
+        /// </summary>
+        祖父母 = 6,
+
+        /// <summary>
+        /// 外祖父母
+        /// </summary>
+        外祖父母 = 601,
+
+        /// <summary>
+        /// 其他
+        /// </summary>
+        其他 = 8,
+
+        /// <summary>
+        /// 女
+        /// </summary>
+        女 = 3,
+
+        /// <summary>
+        /// 兄
+        /// </summary>
+        兄 = 7,
+
+        /// <summary>
+        /// 弟
+        /// </summary>
+        弟 = 701,
+
+        /// <summary>
+        /// 姐
+        /// </summary>
+        姐 = 702,
+
+        /// <summary>
+        /// 妹
+        /// </summary>
+        妹 = 703,
+    }
+}
---
+++ HongMouer.EHR.Models/Enum/EnumUnitAffiliation.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HongMouer.EHR.Models
+{
+
+    /// <summary>
+    ///  单位隶属关系代码  GB/T 12404
+    /// </summary>
+    [Serializable]
+    public enum EnumUnitAffiliation
+    {
+
+        /// <summary>
+        /// 中央
+        /// </summary>
+        中央 = 10,
+
+        /// <summary>
+        /// 省
+        /// </summary>
+        省 = 20,
+
+        /// <summary>
+        /// 市_地区
+        /// </summary>
+        市_地区 = 40,
+
+        /// <summary>
+        /// 街道_镇_乡
+        /// </summary>
+        街道_镇_乡 = 60,
+
+        /// <summary>
+        /// 街道
+        /// </summary>
+        街道 = 61,
+
+        /// <summary>
+        /// 县
+        /// </summary>
+        县 = 50,
+
+        /// <summary>
+        /// 镇
+        /// </summary>
+        镇 = 62,
+
+        /// <summary>
+        /// 居民_村民委员会
+        /// </summary>
+        居民_村民委员会 = 70,
+
+        /// <summary>
+        /// 居民委员会
+        /// </summary>
+        居民委员会 = 71,
+
+        /// <summary>
+        /// 村民委员会
+        /// </summary>
+        村民委员会 = 72,
+
+        /// <summary>
+        /// 乡
+        /// </summary>
+        乡 = 63,
+
+        /// <summary>
+        /// 其他
+        /// </summary>
+        其他 = 90,
+    }
+}
