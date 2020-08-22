62f094df0347cfaf5d1f3aee454eedf8c8b9bbf3
HongMouer <HongMouer@Gmail.com>
2020-08-22 23:54:55 +0800

    Add HealthCheck

---
+++ HongMouer.Common/Middlewares/HealthCheck.cs
@@ -0,0 +1,35 @@
+﻿using HongMouer.EHR.Models;
+using HongMouer.EntityRelationalCore.Repositories;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HongMouer.Common
+{
+    public class HealthCheck : IHealthCheck
+    {
+        private readonly IRepository _Repository;
+
+        public HealthCheck(IRepository repository)
+        {
+            _Repository = repository;
+        }
+
+        public Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
+        {
+            try
+            {
+                _Repository.FindEntity<UserInfo>();
+            }
+            catch (Exception ex)
+            {
+                return Task.FromResult(HealthCheckResult.Unhealthy("From Db Serve"));
+            }
+
+            return Task.FromResult(HealthCheckResult.Healthy());
+        }
+    }
+}
