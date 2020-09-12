166595e778186a315cc157a7f61830e3180bf7b7
HongMouer <HongMouer@Gmail.com>
2020-09-12 22:01:12 +0800

    增加MD5加密方法

---
+++ HongMouer.Common/Utility/MD5Helper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace HongMouer.Common.Utility
+{
+    public sealed class MD5Helper
+    {
+        public static string GetMD5(string str)
+        {
+            using (var md5 = MD5.Create())
+            {
+                var result = md5.ComputeHash(Encoding.UTF8.GetBytes(str));
+                var strResult = BitConverter.ToString(result);
+                return strResult.Replace("-", "");
+            }
+        }
+    }
+}
